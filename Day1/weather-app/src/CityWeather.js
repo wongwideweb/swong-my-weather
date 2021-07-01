6d683ae8593f2bf3f5f8bb9bd930c71779836f44
Steven W <wongwideweb@outlook.com>
2021-07-01 01:19:43 -0400

    scaffold app

---
+++ Day1/weather-app/src/CityWeather.js
@@ -0,0 +1,13 @@
+import React, {Component } from 'react';
+import './App.css';
+import './bootstrap.min.css';
+
+class CityWeather extends Component {
+    render(){
+        return (
+            <div>My Weather</div>
+        );
+    }
+}
+
+export default CityWeather;
